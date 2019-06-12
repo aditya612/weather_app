2fe7706653320e397f371990f92406b1916ba603
unknown <naveenagarwal123451@gmail.com>
2019-06-13 00:56:20 +0530

    fix time

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -8,7 +8,7 @@ class Weather {
   Weather({this.date, this.name, this.temp, this.main, this.icon});
 
   factory Weather.fromJson(Map<String, dynamic> json) => Weather(
-        date: DateTime.fromMillisecondsSinceEpoch(json['dt']),
+        date: DateTime.fromMillisecondsSinceEpoch(json['dt'] * 1000),
         name: json['name'],
         temp: json['main']['temp'].toDouble(),
         main: json['weather'][0]['main'],
