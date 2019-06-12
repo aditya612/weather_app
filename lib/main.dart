b814140bef1768802d310feeb808354a27360602
unknown <naveenagarwal123451@gmail.com>
2019-06-13 01:28:33 +0530

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/Screen/home_page.dart';
+import 'package:weather_app/Screens/home_page.dart';
 
 void main() => runApp(MyApp());
 
